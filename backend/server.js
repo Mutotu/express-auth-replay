1df4418082fcf7204bd4ea7bc4a77719ada624ca
mutotu <muttalip.aydemir@gmail.com>
2021-11-15 22:44:31 -0800

    Muto, express-auth-replay

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,88 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
 
-const rowdy = require ('rowdy-logger')
-const routesReport = rowdy.begin(app)
+const express = require("express");
+const app = express();
 
-app.use(express.json())
-app.use(require('cors')())
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const models = require('./models')
+const rowdy = require("rowdy-logger");
+const routesReport = rowdy.begin(app);
 
-const PORT = process.env.port || 3001
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+app.use(express.json());
+app.use(require("cors")());
+
+const models = require("./models");
+
+const createUser = async (req, res) => {
+  try {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const user = await models.user.create({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const encryptedId = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    // res.json({message: 'ok', user})
+    res.json({ message: "ok", userId: encryptedId });
+  } catch (error) {
+    res.status(400);
+    res.json({ error: "email already taken" });
+  }
+};
+
+app.post("/users", createUser);
+
+const login = async (req, res) => {
+  try {
+    const user = await models.user.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (user.password === req.body.password) {
+      //    res.json({message: 'login successful', user: user})
+      const encryptedId = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+      res.json({ message: "login successful", user: encryptedId });
+    } else {
+      res.status(401);
+      res.json({ error: "login failed" });
+    }
+  } catch (error) {
+    res.status(400);
+    res.json({ error: "login failed" });
+  }
+};
+app.post("/users/login", login);
+
+const userProfile = async (req, res) => {
+  // console.log(req.headers);
+  try {
+    //get the value out of headers instead of <body>
+    //becuase thats where the frontend included it
+    //now that we enncryted the id before sending it to the client, we  need to decrypt it when they send it back
+    const user = await models.user.findOne({
+      where: {
+        // id: req.headers.authorization,
+        id: decryptedId.userId,
+      },
+    });
+    if (user === null) {
+      res.status(404).json({ error: "user not found" });
+    } else {
+      res.json({ message: "user success", user });
+    }
+    //res.json ({message: 'user looked up successfully', user})
+  } catch (error) {
+    res.status(404).json({ error: "user not found" });
+  }
+};
+app.get("/users/profile", userProfile);
+const PORT = process.env.port || 3008;
 app.listen(PORT, () => {
-  routesReport.print()
-})
-
+  routesReport.print();
+});
--- frontend/main.js
+++ frontend/main.js
@@ -1,25 +1,146 @@
 // nav links
-document.querySelector('#home-link').addEventListener('click', () => {
-  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#home-content').classList.remove('hidden')
-})
+document.querySelector("#home-link").addEventListener("click", () => {
+  document
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
+  document.querySelector("#home-content").classList.remove("hidden");
+});
 
-document.querySelector('#signup-link').addEventListener('click', () => {
-  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#signup-content').classList.remove('hidden')
-})
+document.querySelector("#signup-link").addEventListener("click", () => {
+  document
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
+  document.querySelector("#signup-content").classList.remove("hidden");
+});
 
-document.querySelector('#login-link').addEventListener('click', () => {
-  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#login-content').classList.remove('hidden')
-})
+document.querySelector("#login-link").addEventListener("click", () => {
+  document
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
+  document.querySelector("#login-content").classList.remove("hidden");
+});
 
-document.querySelector('#logout-link').addEventListener('click', () => {
-  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#home-content').classList.remove('hidden')
-})
+document.querySelector("#logout-link").addEventListener("click", () => {
+  document
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
+  document.querySelector("#home-content").classList.remove("hidden");
+  localStorage.removeItem("userId");
 
-document.querySelector('#profile-link').addEventListener('click', () => {
-  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#profile-content').classList.remove('hidden')
-})
+  document.querySelector("#signup-link").classList.remove("hidden");
+  document.querySelector("#login-link").classList.remove("hidden");
+  document.querySelector("#logout-link").classList.add("hidden");
+  document.querySelector("#profile-link").classList.add("hidden");
+});
+
+document.querySelector("#profile-link").addEventListener("click", () => {
+  document
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
+  document.querySelector("#profile-content").classList.remove("hidden");
+
+  axios
+    .get("http://localhost:3001/users/profile", {
+      headers: {
+        Authorization: localStorage.getItem("userId"),
+      },
+    })
+    .then((response) => {
+      //console.log(response.data.user.email)
+      document.querySelector(
+        "#profile-info"
+      ).innerText = `Welcome back, ${response.data.user.email}!`;
+    });
+});
+//make a request
+//POST to http://localhost:3001
+//send a body
+//email and password
+//get .value from inputs
+document.querySelector("#signup-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const email = document.querySelector("#signup-email").value;
+    const password = document.querySelector("#signup-password").value;
+
+    /* console.log(email)
+  console.log(password)*/
+    try {
+      const response = await axios.post("http://localhost:3008/users", {
+        email: email,
+        password: password,
+      });
+      console.log(response);
+      const userId = response.data.user.id;
+      localStorage.setItem("userId", userId);
+      addHidden("#signup-link");
+      addHidden("#login-link");
+      removeHidden("#logout-link");
+      removeHidden("#profile-link");
+
+      // document.querySelector("#signup-link").classList.add("hidden");
+      // document.querySelector("#login-link").classList.add("hidden");
+      // document.querySelector("#logout-link").classList.remove("hidden");
+      // document.querySelector("#profile-link").classList.remove("hidden");
+    } catch (error) {
+      console.log(error);
+      alert("email is taken");
+    }
+  });
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const email = document.querySelector("#login-email").value;
+    const password = document.querySelector("#login-password").value;
+
+    /* console.log(email)
+    console.log(password)*/
+    try {
+      const response = await axios.post("http://localhost:3008/users/login", {
+        email: email,
+        password: password,
+      });
+      console.log(response);
+      const userId = response.data.user.id;
+      localStorage.setItem("userId", userId);
+      if (localStorage.getItem("userId")) {
+        addHidden("#signup-link");
+        addHidden("#login-link");
+        removeHidden("#logout-link");
+        removeHidden("#profile-link");
+        // document.querySelector("#signup-link").classList.add("hidden");
+        // document.querySelector("#login-link").classList.add("hidden");
+        // document.querySelector("#logout-link").classList.remove("hidden");
+        // document.querySelector("#profile-link").classList.remove("hidden");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("login failed");
+    }
+  });
+
+if (localStorage.getItem("userId")) {
+  addHidden("#signup-link");
+  addHidden("#login-link");
+  // document.querySelector("#signup-link").classList.add("hidden");
+  // document.querySelector("#login-link").classList.add("hidden");
+} else {
+  addHidden("#logout-link");
+  addHidden("#profile-link");
+  // document.querySelector("#logout-link").classList.add("hidden");
+  // document.querySelector("#profile-link").classList.add("hidden");
+}
+
+function addHidden(id) {
+  document.querySelector(id).classList.add("hidden");
+}
+function removeHidden(id) {
+  document.querySelector(id).classList.remove("hidden");
+}
